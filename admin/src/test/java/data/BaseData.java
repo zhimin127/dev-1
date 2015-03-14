d11cdeb98dfba0f49281a2c6b942554b668d6a66
zhimin127 <zhimin127@sina.com>
2015-03-14 11:28:37 +0800

    废弃

---
+++ admin/src/test/java/data/BaseData.java
@@ -0,0 +1,11 @@
+package data;
+
+public class BaseData {
+	
+	public final static String NAV = "导航";
+	public final static String[] NAV_MENU_ONE = {"资源管理"};
+	public final static String[] NAV_MENU_TWO = {"添加资源"};
+	public final static String[] NAV_MENU_OPT = {"添加资源"};
+	
+
+}
